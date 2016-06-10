28f34c7667d73512e289ef22422bd41165d2b518
mihai bucse <mihaibucse@gmail.com>
2016-06-10 07:25:33 +0100

    sainsburrys - progress

---
+++ database/seeds/ds/Sainsburrys.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace seeds\ds;
+
+class Sainsburrys
+{
+    public $headers = [];
+    public $data = [];
+    public $method = 'GET';
+    public $name = 'Sainsburrys';
+    public $country_code = 'UK';
+    public $url = 'https://api.stores.sainsburys.co.uk/v1/stores/?&store_type=main%2Clocal&within=&sort=&limit=50&offset=0&summary=false';
+}
